ed18b4085be387386158cb27b0020456ca42df16
ahmedassaif <asepsaepul947@gmail.com>
2025-01-01 21:43:16 +0700

    frontend change api address

--- frontend/api/config/config.ts
+++ frontend/api/config/config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
   BASE_URL:
-    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000/api",
+    process.env.NEXT_PUBLIC_API_BASE_URL || "http://192.168.1.15:8000/api",
   TIMEOUT: 30000,
   HEADERS: {
     "Content-Type": "application/json",
