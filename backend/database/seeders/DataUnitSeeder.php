e4901905d3d5027d133bf488f57208f875ba9783
ahmedassaif <ahmedassaif@github.com>
2025-01-03 14:33:56 +0700

    Backend change DataUnit Seeds

--- backend/database/seeders/DataUnitSeeder.php
+++ backend/database/seeders/DataUnitSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\DataUnit;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DataUnitSeeder extends Seeder
 {
@@ -13,38 +13,62 @@ class DataUnitSeeder extends Seeder
      */
     public function run(): void
     {
-        DataUnit::factory()->create(
+        DB::factory('DataUnit')->insert(
             [
                 'NameUnit' => 'Head Office',
                 'Plan' => 'HOF',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'deleted_at' => null,
             ],
             [
                 'NameUnit' => 'Refinery Unit II Dumai',
                 'Plan' => 'K201',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'deleted_at' => null,
             ],
             [
                 'NameUnit' => 'Refinery Unit II Sei Pakning',
                 'Plan' => 'K202',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'deleted_at' => null,
             ],
             [
                 'NameUnit' => 'Refinery Unit III Plaju',
                 'Plan' => 'K203',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'deleted_at' => null,
             ],
             [
                 'NameUnit' => 'Refinery Unit IV Cilacap',
                 'Plan' => 'K204',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'deleted_at' => null,
             ],
             [
                 'NameUnit' => 'Refinery Unit V Balikpapan',
                 'Plan' => 'K205',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'deleted_at' => null,
             ],
             [
                 'NameUnit' => 'Refinery Unit VI Balongan',
                 'Plan' => 'K206',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'deleted_at' => null,
             ],
             [
                 'NameUnit' => 'Refinery Unit VII Kasim',
                 'Plan' => 'K207',
+                'created_at' => now(),
+                'updated_at' => now(),
+                'deleted_at' => null,
             ],
         );
     }
