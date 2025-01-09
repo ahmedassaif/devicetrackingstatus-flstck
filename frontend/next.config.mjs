28fdb13b150c90c090af543434f3b502315db734
ahmedassaif <ahmedassaif@github.com>
2025-01-09 09:32:38 +0700

    perf: (Frontend) enable static export

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', trailingSlash: true, // Enable trailing slash
+};
 
 export default nextConfig;
